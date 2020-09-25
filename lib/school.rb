8bc1671d1be3a0cd78888dcff5f7c68fbcc76f7a
Carl J <carlj1881@yahoo.com>
2020-09-25 05:07:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require"pry"
+
+class School
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+  
+  def grade(grade)
+   
+    if roster[grade]
+    end
+
+  end
+  
+end
