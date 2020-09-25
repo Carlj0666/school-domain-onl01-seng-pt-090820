8b24d549560b1a78991376348764ff7ee91a2edd
Carl J <carlj1881@yahoo.com>
2020-09-25 05:42:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,20 +19,11 @@ class School
   
   def add_student(student, grade)
     if
-      @roster[grade] = []
+      @roster[grade]
       @roster[grade] << student
     else
       @roster[grade] = []
       @roster[grade] << student
-  end
-
-  if @roster[grade]
-[3] pry(#<School>)*   @roster[grade] << student
-[3] pry(#<School>)* else
-[3] pry(#<School>)*   @roster[grade] = []
-[3] pry(#<School>)*   @roster[grade] << student
-[3] pry(#<School>)* end
-
   end
 
   def grade(grade)
